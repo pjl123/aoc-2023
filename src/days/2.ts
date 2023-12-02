4029437228832f56fdc3266c25017c74c34ecc9f
Patrick LaFata <lafatapatrick@gmail.com>
2023-12-02 09:42:35 -0500

    Solves day 2 part 2

--- src/days/2.ts
+++ src/days/2.ts
@@ -2,23 +2,15 @@ export default function solution(input: string): number {
   const lines = input.split('\n').filter(l => l.length > 0);
 
   return lines.map(line => {
-      const id: string = line.split(':')[0].split(' ')[1];
       const rounds: string[] = line.split(': ')[1].split('; ');
-      if (_gameViolatesLimits(rounds)) {
-        return 0;
-      }
-      return Number(id);
+      return _minCubeCounts(rounds);
     })
+    .map(counts => Object.keys(counts).reduce((agg, curr) => agg * counts[curr], 1))
     .reduce((agg, curr) => agg + curr, 0);
 }
 
-const limits = {
-  'red': 12,
-  'green': 13,
-  'blue': 14
-}
-
-function _gameViolatesLimits(rounds: string[]): boolean {
+function _minCubeCounts(rounds: string[]): object {
+  const minCounts = {};
   for (let i = 0; i < rounds.length; i++) {
     const round = rounds[i];
     const marbleCounts = round.split(', ');
@@ -27,10 +19,10 @@ function _gameViolatesLimits(rounds: string[]): boolean {
       const count = Number(countSplit[0]);
       const color = countSplit[1];
 
-      if (limits[color] < count) {
-        return true;
+      if (!(color in minCounts) || minCounts[color] < count) {
+        minCounts[color] = count;
       }
     }
   }
-  return false;
+  return minCounts;
 }
